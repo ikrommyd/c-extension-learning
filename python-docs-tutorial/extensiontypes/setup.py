ae5a5ed10f5876bb590860ff45ad2e6bf036d146
Iason Krommydas <iason.krom@gmail.com>
2025-11-02 23:58:58 -0600

    add sublist

--- python-docs-tutorial/extensiontypes/setup.py
+++ python-docs-tutorial/extensiontypes/setup.py
@@ -9,5 +9,6 @@ setup(
         Extension("custom2", ["custom2.c"]),
         Extension("custom3", ["custom3.c"]),
         Extension("custom4", ["custom4.c"]),
+        Extension("sublist", ["sublist.c"]),
     ]
 )
